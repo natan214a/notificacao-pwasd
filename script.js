23cf86c8e21881d6e1459e9a8f11a9a5ac7e2d14
natan214a <natanshsilva@gmail.com>
2025-08-05 07:47:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function enviarNotificacao() {
   Notification.requestPermission().then(function(result) {
     if (result === 'granted') {
       navigator.serviceWorker.getRegistration().then(function(reg) {
-        reg.showNotification("🚨 Nova Notificação", {
+        reg.showNotification("utmify", {
           body: "Isso aqui é um teste com ícone fake!",
           icon: "https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg",
           image: "https://i.imgur.com/8Km9tLL.jpg",
